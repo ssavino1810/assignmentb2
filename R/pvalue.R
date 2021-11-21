1364391ddb98b954ab3d036709b976d959a1514b
Sara Savino <sarassavino@gmail.com>
2021-11-20 16:44:26 -0800

    chsnges

--- R/pvalue.R
+++ R/pvalue.R
@@ -1,21 +1,25 @@
-#' @title Generate p-value 
-#' @description Returns the p-value for a simple linear regression with 1 independent variable 
+#' @title Determine significance of regression model
+#' @description Interprets the p-value and adjusted r-squared for a regression model
 #' @param x An object of class 'lm', your linear model 
-#' @param if() Logical statement which tests a statement about the object in the brackets
-#' @param p Returns the p-value for the slope coefficient 
+#' @param ifelse() Logical statement which tests a statement about the object in the brackets
+#' @param p Returns the p-value for the slope coefficient
+#' @param q Interprets your p-value at 95% confidence level 
+#' @param r Returns the adjusted R-squared value for your model 
+#' @param s Interprets the adjusted R-squared value for your model  
 #' @param summary() Function that generates the summary table with results from your fitted model 
 #' @param coefficients[] Extracts the p-value for the slope coefficient from the summary table
-#' @return P-value for slope coefficient
+#' @return A vector containing p,q,r and s
 #' @examples 
-#' pvalue(reg1)
 #' pvalue(lm(mtcars$hp ~ mtcars$wt))
 #' @export
 
-pvalue <- function (x) {
+model_sign <- function (x) {
   if (class(x) != "lm") stop("Not an object of class 'lm'. You are using class: ", class(x))
   p <- summary(x)$coefficients[2,4]
   q <- ifelse(p < 0.05, "Congratulations! Your model is significant at 5% significance level.", "Sorry, your model is not significant at 5% significance level")
-  return(q)
+  r <- summary(x)$adj.r.squared
+  s <- c((r*100), "% of variation in Y can be explained by your model")
+  return(c(p, q, r, s))
 }
 
 
--- tests/testthat/test-pvalue.R
+++ tests/testthat/test-pvalue.R
@@ -1,13 +1,15 @@
 test_that("P value is correct", {
   expect_equal(
     summary(lm(mtcars$hp ~ mtcars$wt))$coefficients[2,4], 
-    p_val(reg_3))
+    as.numeric(pvalue(lm(mtcars$hp ~ mtcars$wt))[1]))
 })
 
-test_that("Object is linear model", {
-  expect_is(reg_3, "lm")
+test_that("Adj. R Square is correct", {
+  expect_equal(
+    summary(lm(mtcars$hp ~ mtcars$wt))$adj.r.squared, 
+    as.numeric(pvalue(lm(mtcars$hp ~ mtcars$wt))[3]))
 })
 
 test_that("Error message exists", {
-  expect_error(p_val(test_4),"Not an object of class 'lm'. You are using class: ")
+  expect_error(pvalue(mtcars),"Not an object of class 'lm'. You are using class: ")
 })
